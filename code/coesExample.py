5b733e017a3d16d4935365ea792566b703569b15
Husseinat Etti-Balogun <hebalogun@gmail.com>
2024-03-28 12:03:59 +0900

    abstracted coesexample.py from functions, now in stdtools, confirmed code still works

--- code/coesExample.py
+++ code/coesExample.py
@@ -5,17 +5,6 @@
 import math as m
 import stdlib.stdtools as st
 
-#---------------------------- CONSTANTS ----------------------------------------------------
-
-a = 2160000 #semi major axis in meters
-Kerbin_mass = 5.29e22 # mass of the body in kg
-e = 0.832 # eccentricity
-t = 900 # time elapsed in seconds
-G = 6.67e-11 # universal constant of gravitation
-mu = G * Kerbin_mass # kerbin grav const 
-T = 2 * m.pi * m.sqrt( (a**3) / mu ) # the orbital period of rocket around Kerbin in days
-n = ( 2 * m.pi ) / T # mean motion of the rocket rad/sec
-
 #---------------------------- MAIN CODE ----------------------------------------------------
 
 if __name__ == '__main__':
